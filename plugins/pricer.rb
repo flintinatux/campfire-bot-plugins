6b4c43656cc5bb7f72b09dc0131bd6296b52fa6c
Scott McCormack <flintinatux@gmail.com>
2012-11-08 15:34:50 -0500

    First stab at pulling gold price

---
+++ plugins/pricer.rb
@@ -0,0 +1,18 @@
+require 'nokogiri'
+
+class Pricer < CampfireBot::Plugin
+  on_message %r[(price of gold|gold price)], :gold_price
+
+  def initialize
+    @log = Logging.logger["CampfireBot::Plugin::Pricer"]
+  end
+
+  def gold_price(m)
+    url = 'http://www.kitco.com/market/'
+    doc = Nokogiri::HTML(open(url))
+    tr = doc.css('tr[bgcolor="#F3F3E4"][align="center"]')[4]
+    p = tr.css 'td p'
+    price = "GOLD - bid: #{p[3].content} ask: #{p[4].content} low: #{p[7].content} hi: #{p[8].content} - #{p[1].content} #{p[2].content} src: #{url}"
+    m.speak price
+  end
+end
